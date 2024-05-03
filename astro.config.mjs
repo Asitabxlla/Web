da02914fcbf5d643bd86398316af372b3dbec0a5
Asia <v.poza.2021@alumnos.urjc.es>
2024-05-03 03:22:05 +0200

    arreglado swiperempezado lineart

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
+import Swiper from 'swiper';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+import { Navigation, Pagination } from 'swiper/modules';
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(),],
   output: "server",
   adapter: vercel()
 });
